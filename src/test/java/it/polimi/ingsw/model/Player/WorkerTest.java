b8e7fa2f8882b012ac193bbaa1c2f6e77efdc76a
Scilux <scilu@live.com>
2020-03-19 20:05:58 +0100

    TestWorker

--- src/test/java/it/polimi/ingsw/model/Player/WorkerTest.java
+++ src/test/java/it/polimi/ingsw/model/Player/WorkerTest.java
@@ -1,20 +1,29 @@
 package it.polimi.ingsw.model.Player;
 
 
+import it.polimi.ingsw.model.Map.Building;
+import it.polimi.ingsw.model.Map.MapLoader;
 import it.polimi.ingsw.model.Map.Square;
 import it.polimi.ingsw.model.Player.Worker;
 import it.polimi.ingsw.model.Player.WorkerName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkerTest {
-    Worker worker;
-    Square square, square1, square2;
+
+
+
 
     @Test
     void getBoardPosition() {
-         worker = new Worker(WorkerName.WORKER1, square, square1, square2);
+
+        Worker worker;
+        ArrayList<Square> squares = MapLoader.loadMap();
+        worker = new Worker(WorkerName.WORKER1, squares.get(0), squares.get(1), squares.get(3));
         assert (worker.getBoardPosition() != null);
     }
 
@@ -40,7 +49,9 @@ class WorkerTest {
 
     @Test
     void getName() {
-        worker = new Worker(WorkerName.WORKER1, square, square1, square2);
+        Worker worker;
+        ArrayList<Square> squares = MapLoader.loadMap();
+       worker = new Worker(WorkerName.WORKER1, squares.get(1), squares.get(2), squares.get(3));
         assert (worker.getName() != null);
     }
 }
