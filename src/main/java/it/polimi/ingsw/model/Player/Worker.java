d4fc394517788877e675bf7499f64fceb1d50788
Scilux <scilu@live.com>
2020-03-19 12:48:59 +0100

    Attributi + getter e setter classe Worker

--- src/main/java/it/polimi/ingsw/model/Player/Worker.java
+++ src/main/java/it/polimi/ingsw/model/Player/Worker.java
@@ -1,4 +1,32 @@
 package it.polimi.ingsw.model.Player;
 
+import it.polimi.ingsw.model.Map.Square;
+
 public class Worker {
+
+    private WorkerName name;
+    private Square boardPosition;
+    private  Square previousBoardPosition;
+    private  Square previousBuilPosition;
+
+    public Worker(WorkerName name, Square boardPosition, Square previousBoardPosition, Square previousBuilPosition){
+
+        this.name = name;
+        this.boardPosition = boardPosition;
+        this.previousBoardPosition = previousBoardPosition;
+        this.previousBuilPosition = previousBuilPosition;
+    }
+
+    public Square getBoardPosition() { return boardPosition;}
+
+    public void setBoardPosition(Square boardPosition) { this.boardPosition = boardPosition;}
+
+    public Square getPreviousBoardPosition() { return previousBoardPosition;}
+
+    public void setPreviousBoardPosition(Square previousBoardPosition) { this.previousBoardPosition = previousBoardPosition;}
+
+    public Square getPreviousBuilPosition() { return previousBuilPosition;}
+
+    public void setPreviousBuilPosition(Square previousBuilPosition) { this.previousBuilPosition = previousBuilPosition;}
+
 }
