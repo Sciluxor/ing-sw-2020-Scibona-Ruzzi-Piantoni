b2425b7693e9e7c4eae2ee12ddf20e8289082390
alessandroruzzi <alrxruzzi98@gmail.com>
2020-03-20 17:49:40 +0100

    Implemented Queue for Player Turn

--- src/main/java/it/polimi/ingsw/model/Player/PlayerQueue.java
+++ src/main/java/it/polimi/ingsw/model/Player/PlayerQueue.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw.model.Player;
 
-public class PlayerQueue {
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Queue;
+
+public class PlayerQueue extends ArrayDeque<Player> {
+
+
+    public PlayerQueue(ArrayList<Player> players) {
+        super(players);
+    }
+
+
+    public void changeTurn(){
+        if(!isEmpty()){
+            addLast(removeFirst());
+        }
+    }
 }
