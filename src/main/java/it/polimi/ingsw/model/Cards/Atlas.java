0500963fb9157b1807a91739aa48ebffd7b06652
Scilux <scilu@live.com>
2020-03-22 19:33:15 +0100

    Carta Atlas

--- src/main/java/it/polimi/ingsw/model/Cards/Atlas.java
+++ src/main/java/it/polimi/ingsw/model/Cards/Atlas.java
@@ -1,7 +1,33 @@
 package it.polimi.ingsw.model.Cards;
 
+import it.polimi.ingsw.model.Map.Building;
+import it.polimi.ingsw.model.Map.Directions;
+import it.polimi.ingsw.model.Map.GameMap;
+import it.polimi.ingsw.model.Map.Square;
+import it.polimi.ingsw.model.Player.Worker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Atlas extends Card {
     public Atlas(String name, String description, boolean isPlayableIn3, CardType type, CardSubType subType) {
         super(name, description, isPlayableIn3, type, subType);
     }
+    @Override
+    public Response executeBuild(GameMap gameMap, Building building, Directions directions, Worker worker) {
+        if(gameMap == null || worker == null || building == null || directions == null)
+            throw new NullPointerException("null gameMap or worker or building or direction");
+
+        Square buildingSquare = gameMap.getGameMap().get(worker.getBoardPosition().getCanAccess().get(directions) -1);
+        if(building.equals(Building.mapNext(buildingSquare.getBuilding())) || building.equals(Building.DOME)){
+            worker.setPreviousBuildPosition(buildingSquare);
+            buildingSquare.setBuilding(building);
+            buildingSquare.addBuildingLevel();
+            return Response.BUILD;
+        }
+        return Response.NOTBUILD;
+    }
+
+
+
 }
