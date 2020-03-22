8208a3915e52c29fd337fd6aaaa9949c7a771687
Scilux <scilu@live.com>
2020-03-22 16:50:36 +0100

    Completamento classe PlayerQueueTest

--- src/test/java/it/polimi/ingsw/model/Player/PlayerQueueTest.java
+++ src/test/java/it/polimi/ingsw/model/Player/PlayerQueueTest.java
@@ -13,6 +13,10 @@ class PlayerQueueTest {
 
     @Test
     void changeTurn() {
+        players = new ArrayList<>();
+        player1 = new Player("GoodPlayer", TurnStatus.PREGAME);
+        player2 = new Player("GoodPlayer", TurnStatus.PREGAME);
+        player3 = new Player("GoodPlayer", TurnStatus.PREGAME);
         players.add(player1);
         players.add(player2);
         players.add(player3);
@@ -20,6 +24,15 @@ class PlayerQueueTest {
         assertEquals(players.get(0), player1);
         assertEquals(players.get(1), player2);
         assertEquals(players.get(2), player3);
-        changeTurn();
+        PlayerQueue queue = new PlayerQueue(players);
+        assertEquals(queue.size(), 3);
+        assertEquals(queue.peek(), player1);
+        queue.changeTurn();
+        assertEquals(queue.peek(), player2);
+        queue.changeTurn();
+        assertEquals(queue.peek(), player3);
+        queue.changeTurn();
+        assertEquals(queue.peek(), player1);
+
     }
 }
