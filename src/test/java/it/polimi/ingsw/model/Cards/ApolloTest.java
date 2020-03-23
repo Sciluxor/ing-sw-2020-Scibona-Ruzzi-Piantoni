35adda47040ceb737b87710c65812b4395357f79
Scilux <scilu@live.com>
2020-03-23 21:58:48 +0100

    Aggiornamento classe ApolloTest in attesa di modifica Apollo

--- src/test/java/it/polimi/ingsw/model/Cards/ApolloTest.java
+++ src/test/java/it/polimi/ingsw/model/Cards/ApolloTest.java
@@ -1,9 +1,15 @@
 package it.polimi.ingsw.model.Cards;
 
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Map.Directions;
+import it.polimi.ingsw.model.Map.GameMap;
 import it.polimi.ingsw.model.Map.MapLoader;
 import it.polimi.ingsw.model.Map.Square;
+import it.polimi.ingsw.model.Player.Player;
+import it.polimi.ingsw.model.Player.TurnStatus;
 import it.polimi.ingsw.model.Player.Worker;
 import it.polimi.ingsw.model.Player.WorkerName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,21 +17,43 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApolloTest {
+
+    Player player1, player2;
     Card cardAp;
-    Worker worker1 = new Worker(WorkerName.WORKER1);
-    Worker worker2 = new Worker(WorkerName.WORKER2);
-    ArrayList<Square> squares;
+    GameMap gameMap;
+
+
+
+    @BeforeEach
+    void setup(){
+        player1 = new Player("uno", TurnStatus.PREGAME);
+        player2 = new Player("due", TurnStatus.PREGAME);
+        cardAp = CardLoader.loadCards().get("Apollo");
+        player1.setPower(cardAp);
+        gameMap = new GameMap();
+        gameMap.getGameMap().get(22).setMovement(player1,player1.getWorkers().get(0));
+        player1.getWorkers().get(0).setBoardPosition(gameMap.getGameMap().get(22));
+        gameMap.getGameMap().get(4).setMovement(player1,player1.getWorkers().get(1));
+        player1.getWorkers().get(1).setBoardPosition(gameMap.getGameMap().get(4));
+        gameMap.getGameMap().get(21).setMovement(player2,player2.getWorkers().get(0));
+        player2.getWorkers().get(0).setBoardPosition(gameMap.getGameMap().get(21));
+        gameMap.getGameMap().get(18).setMovement(player2,player2.getWorkers().get(1));
+        player2.getWorkers().get(1).setBoardPosition(gameMap.getGameMap().get(18));
+        player1.selectCurrentWorker(gameMap, "worker1");
+        player2.selectCurrentWorker(gameMap, "worker2");
+    }
+
+
+
 
     @Test
-    void swapWorker() {
-        cardAp = CardLoader.loadCards().get("Athena");
-        squares = MapLoader.loadMap();
-        worker1.setBoardPosition(squares.get(0));
-        worker2.setBoardPosition(squares.get(1));
-        assertEquals(worker1.getBoardPosition(), squares.get(0));
-        assertEquals(worker2.getBoardPosition(), squares.get(1));
-        cardAp.swapWorker(worker1, worker2);
-        assertEquals(worker1.getBoardPosition(), squares.get(1));
-        assertEquals(worker2.getBoardPosition(), squares.get(0));
+    void executeWorkerMove() {
+
+
+        assertEquals(player1.getCurrentWorker().getBoardPosition(), gameMap.getGameMap().get(22));
+        assertEquals(player2.getCurrentWorker().getBoardPosition(), gameMap.getGameMap().get(21));
+        player1.executeWorkerMove(gameMap, Directions.EST);
+        assertEquals(player1.getCurrentWorker().getBoardPosition(), gameMap.getGameMap().get(21));
+        assertEquals(player2.getCurrentWorker().getBoardPosition(), gameMap.getGameMap().get(22));
     }
 }
---
+++ src/test/java/it/polimi/ingsw/model/Cards/ArtemisTest.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.model.Cards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtemisTest {
+
+    @Test
+    void findWorkerMove() {
+    }
+
+    @Test
+    void notPreviousMove() {
+    }
+
+    @Test
+    void executeWorkerMove() {
+    }
+}
