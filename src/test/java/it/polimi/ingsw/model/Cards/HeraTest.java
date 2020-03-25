adbf576700ea58b170d8eb39914457241922f057
Scilux <scilu@live.com>
2020-03-25 18:57:52 +0100

    Realizazione HereTest

---
+++ src/test/java/it/polimi/ingsw/model/Cards/HeraTest.java
@@ -0,0 +1,54 @@
+package it.polimi.ingsw.model.Cards;
+
+import it.polimi.ingsw.model.Map.Directions;
+import it.polimi.ingsw.model.Map.GameMap;
+import it.polimi.ingsw.model.Player.Player;
+import it.polimi.ingsw.model.Player.TurnStatus;
+import it.polimi.ingsw.model.Player.Worker;
+import it.polimi.ingsw.model.Player.WorkerName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeraTest {
+
+    Player player1, player2;
+    Card cardHera;
+    Worker worker1,worker2;
+    GameMap gameMap;
+    ArrayList<Directions> directions;
+
+    @BeforeEach
+    void setup(){
+        player1 = new Player("GoodPlayer", TurnStatus.PREGAME);
+        player2 = new Player("BadPlayer", TurnStatus.PREGAME);
+        cardHera = CardLoader.loadCards().get("Hera");
+        player1.setPower(cardHera);
+        worker1 = new Worker(WorkerName.WORKER1);
+        worker2 = new Worker(WorkerName.WORKER2);
+        gameMap = new GameMap();
+        gameMap.getGameMap().get(22).setMovement(player1,player1.getWorkers().get(0));
+        player1.getWorkers().get(0).setBoardPosition(gameMap.getGameMap().get(22));
+        gameMap.getGameMap().get(4).setMovement(player1,player1.getWorkers().get(1));
+        player1.getWorkers().get(1).setBoardPosition(gameMap.getGameMap().get(4));
+        gameMap.getGameMap().get(21).setMovement(player2,player2.getWorkers().get(0));
+        player2.getWorkers().get(0).setBoardPosition(gameMap.getGameMap().get(21));
+        gameMap.getGameMap().get(18).setMovement(player2,player2.getWorkers().get(1));
+        player2.getWorkers().get(1).setBoardPosition(gameMap.getGameMap().get(18));
+        player1.selectCurrentWorker(gameMap, "worker1");
+        directions = player1.findWorkerMove(gameMap, player1.getWorkers().get(0));
+    }
+
+    @Test
+    void isValidVictory() {
+        assertThrows(NullPointerException.class , () -> cardHera.findPossibleBuild(null, worker1));
+        assertThrows(NullPointerException.class , () -> cardHera.findPossibleBuild(gameMap, null));
+
+        assertTrue(cardHera.isValidVictory(gameMap, player1.getCurrentWorker()));
+        player1.getCurrentWorker().setBoardPosition(gameMap.getGameMap().get(13));
+        assertFalse(cardHera.isValidVictory(gameMap, player1.getCurrentWorker()));
+    }
+}
