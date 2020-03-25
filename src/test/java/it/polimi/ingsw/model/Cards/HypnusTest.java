188385ca47cde3b147a3a517d38e9f84dbf9d0d2
Scilux <scilu@live.com>
2020-03-25 19:26:20 +0100

    Realizazione HypnusTest

---
+++ src/test/java/it/polimi/ingsw/model/Cards/HypnusTest.java
@@ -0,0 +1,56 @@
+package it.polimi.ingsw.model.Cards;
+
+import it.polimi.ingsw.model.Map.Directions;
+import it.polimi.ingsw.model.Map.GameMap;
+import it.polimi.ingsw.model.Player.Player;
+import it.polimi.ingsw.model.Player.TurnStatus;
+import it.polimi.ingsw.model.Player.Worker;
+import it.polimi.ingsw.model.Player.WorkerName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HypnusTest {
+
+    Player player1, player2;
+    Card cardHypn;
+    Worker worker1,worker2;
+    GameMap gameMap;
+    ArrayList<Directions> directions;
+
+    @BeforeEach
+    void setup(){
+        player1 = new Player("GoodPlayer", TurnStatus.PREGAME);
+        player2 = new Player("BadPlayer", TurnStatus.PREGAME);
+        cardHypn = CardLoader.loadCards().get("Hypnus");
+        player1.setPower(cardHypn);
+        worker1 = new Worker(WorkerName.WORKER1);
+        worker2 = new Worker(WorkerName.WORKER2);
+        gameMap = new GameMap();
+        gameMap.getGameMap().get(22).setMovement(player1,player1.getWorkers().get(0));
+        player1.getWorkers().get(0).setBoardPosition(gameMap.getGameMap().get(22));
+        gameMap.getGameMap().get(4).setMovement(player1,player1.getWorkers().get(1));
+        player1.getWorkers().get(1).setBoardPosition(gameMap.getGameMap().get(4));
+        gameMap.getGameMap().get(21).setMovement(player2,player2.getWorkers().get(0));
+        player2.getWorkers().get(0).setBoardPosition(gameMap.getGameMap().get(21));
+        gameMap.getGameMap().get(18).setMovement(player2,player2.getWorkers().get(1));
+        player2.getWorkers().get(1).setBoardPosition(gameMap.getGameMap().get(18));
+        player1.selectCurrentWorker(gameMap, "worker1");
+        directions = player1.findWorkerMove(gameMap, player1.getWorkers().get(0));
+    }
+
+    @Test
+    void canMove() {
+        assertThrows(NullPointerException.class , () -> cardHypn.canMove(null, worker1));
+        assertThrows(NullPointerException.class , () -> cardHypn.canMove(player1, null));
+
+        assertTrue(cardHypn.canMove(player1, player1.getCurrentWorker()));
+        gameMap.getGameMap().get(4).addBuildingLevel();
+        assertEquals(gameMap.getGameMap().get(4).getBuildingLevel(), 1);
+        //assertFalse(cardHypn.canMove(player1, player1.getCurrentWorker()));
+
+    }
+}
