6c8c928c08962e1332320b7592a61c08acf32f99
Scilux <scilu@live.com>
2020-04-21 16:25:38 +0200

    Login

---
+++ src/main/java/it/polimi/ingsw/view/Client/GUI/Login.java
@@ -0,0 +1,65 @@
+package it.polimi.ingsw.view.Client.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class Login extends JPanel{
+    private JTextField nickname;
+    Dimension frameSize = new Dimension();
+
+
+
+
+    public Login(Dimension screen, Dimension frame) throws IOException {
+
+        frameSize.setSize(frame);
+        setPreferredSize(frameSize);
+        setLayout(null);
+
+        JButton confirm = new JButton("Confirm");
+        nickname = new JTextField(20);
+        JTextField numberPlayers = new JTextField(20);
+        JLabel santoriniLabel = new JLabel("Santorini");
+        JLabel nicknameLabel = new JLabel("Nickname");
+        JLabel numberPlayersLabel = new JLabel("Number of Players");
+        double ratio = (screen.getWidth()/screen.getHeight());
+
+        JLabel cover = ImageHandler.setImage("resources/background_login.png", 100, 100, frameSize.width, frameSize.height);
+        JLabel sfondo = new JLabel(cover.getIcon());
+
+        JButton back = new JButton();
+        back.setIcon(sfondo.getIcon());
+        back.setBounds(0, 0, frameSize.width, frameSize.height);
+        back.setOpaque(false);
+        back.setContentAreaFilled(false);
+        back.setBorderPainted(false);
+
+
+
+        santoriniLabel.setBounds(frameSize.width * 50/100 - (screen.width * 4/100)/2, frameSize.height * 20/100 - (screen.height * 4/100)/2, screen.width * 10/100,screen.height*10/100);
+        add(santoriniLabel);
+
+        confirm.setBounds(frameSize.width * 50/100 - (screen.width * 10/100)/2, frameSize.height * 82/100 - 50, screen.width * 10/100, screen.height * 3/100);
+        confirm.setOpaque(false);
+        confirm.setContentAreaFilled(false);
+        confirm.setFocusPainted(false);
+        add(confirm);
+
+        nickname.setBounds(frameSize.width * 47/100 - (screen.width * 6/100)/2, frameSize.height * 45/100 - (screen.height * 6/100)/2, screen.width * 12/100,screen.height * 3/100);
+        add(nickname);
+        nicknameLabel.setBounds(frameSize.width * 37/100 - (screen.width * 6/100)/2, frameSize.height * 45/100 - (screen.height * 6/100)/2, screen.width * 12/100,screen.height * 3/100);
+        add(nicknameLabel);
+
+        numberPlayers.setBounds(frameSize.width * 47/100 - (screen.width * 6/100)/2, frameSize.height * 55/100 - (screen.height * 6/100)/2, screen.width * 12/100,screen.height * 3/100);
+        add(numberPlayers);
+        numberPlayersLabel.setBounds(frameSize.width * 37/100 - (screen.width * 6/100)/2, frameSize.height * 55/100 - (screen.height * 6/100)/2, screen.width * 12/100,screen.height * 3/100);
+        add(numberPlayersLabel);
+
+        add(back);
+        confirm.addActionListener(new Gui.ChangePanel());
+
+    }
+}
