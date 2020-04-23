991d7add9895bbce7ded56d06a807aefb7069048
edoardopiantoni <edoardo.piantoni@mail.polimi.it>
2020-04-23 12:19:41 +0200

    Elimino App interna a view e sposto il suo codice in App esterna ai package

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,15 +1,27 @@
 package it.polimi.ingsw;
 
 
+import it.polimi.ingsw.view.client.cli.Cli;
+import it.polimi.ingsw.view.client.gui.Gui;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Insert 1 to run CLI, insert 2 to run GUI: ");
+        String keyboard = input.nextLine();
+        if(keyboard.equals("1")) {
+            Cli cli = new Cli();
+            cli.avvio();
+        }
+        else if(keyboard.equals("2")) {
+            Gui gui = new Gui();
+            gui.avvio();
+        }
+
     }
+
 }
