a06ea611aedef1a60d50de55d28184c0372a5b06
Scilux <scilu@live.com>
2020-06-01 20:28:29 +0200

    Correzione wrong build

--- src/main/java/it/polimi/ingsw/model/cards/Prometheus.java
+++ src/main/java/it/polimi/ingsw/model/cards/Prometheus.java
@@ -71,7 +71,7 @@ public class Prometheus extends Card {
     public boolean wrongBuild(GameMap gameMap,Worker worker,Directions directions){
         List<Directions> possibleMove = notUpMove(gameMap,worker);
         return possibleMove.size() <= 1 && (possibleMove.size() != 1 || (possibleMove.get(0).equals(directions) &&
-                gameMap.getMap().get(worker.getBoardPosition().getCanAccess().get(directions) -1).getBuildingLevel() < worker.getBoardPosition().getBuildingLevel()));
+                gameMap.getMap().get(worker.getBoardPosition().getCanAccess().get(directions) -1).getBuildingLevel() >= worker.getBoardPosition().getBuildingLevel()));
     }
 
     @Override
