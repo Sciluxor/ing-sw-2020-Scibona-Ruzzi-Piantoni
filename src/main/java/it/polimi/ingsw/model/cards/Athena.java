93c0d6e17bbf3601bbe6e69e23e9307a13821f69
Scilux <scilu@live.com>
2020-06-27 23:08:57 +0200

    JavaDoc Athena

--- src/main/java/it/polimi/ingsw/model/cards/Athena.java
+++ src/main/java/it/polimi/ingsw/model/cards/Athena.java
@@ -11,7 +11,22 @@ import it.polimi.ingsw.model.Response;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Luigi Scibona, Alessandro Ruzzi, Edoardo Piantoni
+ * @version 1.0
+ * @since 2020/06/27
+ */
+
 public class Athena extends Card {
+
+    /**
+     * Class Builder
+     * @param name Name of the card
+     * @param description Description of the power of the card
+     * @param isPlayableIn3 Boolean saying if the card is playable in 3 Players
+     * @param type Type of the card
+     * @param subType Subtype of the card
+     */
 
     public Athena(String name, String description, boolean isPlayableIn3, CardType type, CardSubType subType) {
         super(name, description, isPlayableIn3, type, subType);
