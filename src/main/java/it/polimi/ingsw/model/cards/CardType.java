665d0a141ecac465886ca4ed157760e7c9774ae4
Scilux <scilu@live.com>
2020-06-27 23:59:31 +0200

    JavaDoc CardType

--- src/main/java/it/polimi/ingsw/model/cards/CardType.java
+++ src/main/java/it/polimi/ingsw/model/cards/CardType.java
@@ -1,5 +1,12 @@
 package it.polimi.ingsw.model.cards;
 
+/**
+ * Enumeration of the card Types
+ * @author Luigi Scibona, Alessandro Ruzzi, Edoardo Piantoni
+ * @version 1.0
+ * @since 2020/06/27
+ */
+
 public enum CardType {
     YOURMOVE, BUILDVICTORY, MOVEVICTORY, YOURBUILD, YOURTURN
 }
