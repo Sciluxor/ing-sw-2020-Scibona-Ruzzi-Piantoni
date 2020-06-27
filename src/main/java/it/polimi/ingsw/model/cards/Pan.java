cf8b2c4babec8a882f3e57da2a314c27f9b03aa5
Scilux <scilu@live.com>
2020-06-28 00:42:22 +0200

    JavaDoc Pan

--- src/main/java/it/polimi/ingsw/model/cards/Pan.java
+++ src/main/java/it/polimi/ingsw/model/cards/Pan.java
@@ -5,7 +5,23 @@ import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.Response;
 import it.polimi.ingsw.utils.ConstantsContainer;
 
+/**
+ * Class that extends Card that build the Pan card
+ * @author Luigi Scibona, Alessandro Ruzzi, Edoardo Piantoni
+ * @version 1.0
+ * @since 2020/06/27
+ */
+
 public class Pan extends Card {
+
+    /**
+     * Class Builder
+     * @param name Name of the card
+     * @param description Description of the power of the card
+     * @param isPlayableIn3 Boolean saying if the card is playable in 3 Players
+     * @param type Type of the card
+     * @param subType Subtype of the card
+     */
 
     public Pan(String name, String description, boolean isPlayableIn3, CardType type, CardSubType subType) {
         super(name, description, isPlayableIn3, type, subType);
